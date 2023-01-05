f6ec273693ca0682d61c6bfb2ae612639e8cddc0
Magic <104694012+anonfoxer2@users.noreply.github.com>
2023-01-05 01:00:50 -0600

    version 1.0.0 - spnati cheat

--- mature/novicane.js
+++ mature/novicane.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Novicane - SPNATI Cheat
-// @namespace    https://www.github.com/anonfoxer
-// @version      0.3.0
+// @namespace    https://www.github.com/anonfoxer2
+// @version      1.0.0
 // @description  Collection of cheats for the browser game SPNATI, put into a large userscript for ease of use
 // @author       anonfoxer
 // @match        https://spnati.net/*
@@ -17,7 +17,7 @@ console.log("N O V I C A N E _ L O A D I N G . . .");
 
 //cheat name button because flair is important.
 var cheat = document.createElement("button");
-cheat.innerHTML = "Novicane v0.3.0";
+cheat.innerHTML = "Novicane v1.0.0";
 var gameVers = document.getElementById("title-version-button");
 try {
     gameVers.appendChild(cheat);
@@ -37,36 +37,34 @@ splashButton.innerHTML = "Info";
 document.getElementsByClassName("optional-bordered player-card-area")[0].appendChild(splashButton);
 
 //instant epilogue button
-/*
 var instantEpilogueButton = document.createElement("button");
-instantEpilogueButton.innerHTML = "Instant Epilogue";
-document.body.appendChild(instantEpilogueButton);
-*/
+instantEpilogueButton.innerHTML = "Epilogue";
+document.getElementsByClassName("optional-bordered player-card-area")[0].appendChild(instantEpilogueButton);
+
 
 //splash listener
 splashButton.addEventListener ("click", function() {
-    alert("Novicane v0.3.0 by anonfoxer");
-    alert("Changes: -Removed debug mode button (doesnt work online)\n-Added cheat splash and information.\n-Updated button appending to make buttons appear.");
+    alert("Novicane v1.0.0 by anonfoxer");
+    alert("Changes!\n-Removed debug mode button (doesnt work online)\n-Added cheat splash and information.\n-Updated button appending to make buttons appear.\n-Added instant epilogue button.\n\n\nUsage:\nClick win hand at any time to win the hand instantly.\nClick Epilogue to immediately jump to the epilogues that are available with the characters you\'ve been playing against.\nClick info to show this dialogue.");
 });
 
 //win hand listener
 winHandButton.addEventListener ("click", function() {
 try {
-    console.log("Giving player winning hand");
+    console.log("Novicane >> Giving player winning hand");
     players[0].hand.cards = [ 14, 13, 12, 11, 10 ].map(function(n) { return new Card(0 - 1, n); });
 }
     catch (e) {
-    console.log("An error occurred when trying to force a winning hand. Error:");
+    console.log("Novicane >> An error occurred when trying to force a winning hand. Error:");
     console.log(e);
     }
 });
 
 
-//instant epilogue listener
-/*
+
 instantEpilogueButton.addEventListener ("click", function() {
-    var character = prompt("Enter the name of the character you want to see the epilogue of. They must have an epilogue written.","raven");
-    console.log(character);
-    doEpilogueModal(character);
+    console.log("Novicane >> Forcing epilogue modal...");
+    doEpilogueModal();
 });
-*/
+
+console.log("Novicane >> Loaded and functioning properly.");
