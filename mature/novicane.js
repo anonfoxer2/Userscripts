ed678ad215efb1a8eef25a8fb2f8e8491f906e22
Magic <104694012+anonfoxer2@users.noreply.github.com>
2023-01-06 13:19:35 -0600

    v1.0.1. typo changes and such.

--- mature/novicane.js
+++ mature/novicane.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Novicane - SPNATI Cheat
-// @namespace    https://www.github.com/anonfoxer
-// @version      1.0.0
+// @name         Novocaine - SPNATI Cheat
+// @namespace    https://www.github.com/anonfoxer2
+// @version      1.0.1
 // @description  Collection of cheats for the browser game SPNATI, put into a large userscript for ease of use
 // @author       anonfoxer
 // @match        https://spnati.net/*
@@ -9,10 +9,15 @@
 // @license      MIT
 // ==/UserScript==
 
+/* Changes in 1.0.1:
+- Fixed the typo of the name (finally)
+- Added proper try..catch error handling where needed.*/
+
 
 /* Making the buttons and appending them all
 just underneath the name bubble of each character.
-Kinda roundabout but eh. 
+Kinda roundabout but eh.
+
 Yes i am well aware debug mode lets you do all of these things but
 1. it only really works in offline
 2. doing it in the online version is tedious even with a script like this
@@ -22,20 +27,17 @@ Yes i am well aware debug mode lets you do all of these things but
 im sick of people discrediting work like this because "Oh well debug mode and offline version"
 yes, that is an option, but please stop pretending like stuff like this isn't useful.*/
 
-console.log("N O V I C A N E _ L O A D I N G . . .");
+console.log("N O V O C A I N E _ L O A D I N G . . .");
 
-//cheat name button because flair is important.
+//cheat name button because flair is important. Appends itself underneath the button that displays game version.
+//Clicking it just displays the game version info.
 var cheat = document.createElement("button");
-cheat.innerHTML = "Novicane v1.0.0";
+cheat.innerHTML = "Novocaine v1.0.1";
 var gameVers = document.getElementById("title-version-button");
-try {
-    gameVers.appendChild(cheat);
-}
-catch(e) {
-console.log(e);
-}
+gameVers.appendChild(cheat);
 
-//win hand button
+
+//win hand button. all buttons from here down append to the card display area.
 var winHandButton = document.createElement("button");
 winHandButton.innerHTML = "Win Hand";
 document.getElementsByClassName("optional-bordered player-card-area")[0].appendChild(winHandButton);
@@ -53,27 +55,40 @@ document.getElementsByClassName("optional-bordered player-card-area")[0].appendC
 
 //splash listener
 splashButton.addEventListener ("click", function() {
-    alert("Novicane v1.0.0 by anonfoxer");
-    alert("Changes!\n-Removed debug mode button (doesnt work online)\n-Added cheat splash and information.\n-Updated button appending to make buttons appear.\n-Added instant epilogue button.\n\n\nUsage:\nClick win hand at any time to win the hand instantly.\nClick Epilogue to immediately jump to the epilogues that are available with the characters you\'ve been playing against.\nClick info to show this dialogue.");
+try {
+    console.log("Novocaine >> Displaying cheat splash...");
+    alert("Novocaine v1.0.1 by anonfoxer");
+    alert("Changes!\n-Fixed the typo in the name.\n-Added proper try..catch error handling where needed.\n\n\nUsage:\nClick win hand at any time to win the hand instantly.\nClick Epilogue to immediately jump to the epilogues that are available with the characters you\'ve been playing against.\nClick info to show this dialogue.");
+}
+catch (e) {
+    console.log("Novocaine >> Error when displaying cheat splash. Error:");
+    console.log(e);
+}
 });
 
 //win hand listener
 winHandButton.addEventListener ("click", function() {
 try {
-    console.log("Novicane >> Giving player winning hand");
+    console.log("Novocaine >> Giving player winning hand...");
     players[0].hand.cards = [ 14, 13, 12, 11, 10 ].map(function(n) { return new Card(0 - 1, n); });
 }
     catch (e) {
-    console.log("Novicane >> An error occurred when trying to force a winning hand. Error:");
+    console.log("Novocaine >> An error occurred when trying to force a winning hand. Error:");
     console.log(e);
     }
 });
 
 
-
+//instant epilogue listener
 instantEpilogueButton.addEventListener ("click", function() {
-    console.log("Novicane >> Forcing epilogue modal...");
+try {
+    console.log("Novocaine >> Forcing epilogue modal...");
     doEpilogueModal();
+}
+catch (e) {
+    console.log("Novocaine >> Error when forcing epilogue dialogue box. Error:");
+    console.log(e);
+}
 });
 
-console.log("Novicane >> Loaded and functioning properly.");
+console.log("Novocaine >> Loaded and functioning properly.");
